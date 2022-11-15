986ef43ba2d1ffa92cce40a352ad905586757680
marikomands <marikomands@gmail.com>
2022-11-15 12:53:18 +0900

    added ClothesDetail component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { selectClothes } from "../actions";
+// import { selectClothes } from "../actions";
 import ClothesList from "../components/ClothesList";
+import ClothesDetail from "../components/ClothesDetail";
+// import AllClothesList from "../components/AllClothesList";
 
 const App = () => {
   return (
     <div>
       <ClothesList />
+      <ClothesDetail />
     </div>
   );
 };
