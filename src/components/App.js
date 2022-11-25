34773c578e5c0f044e36cbb5dad5a1a663824613
marikomands <marikomands@gmail.com>
2022-11-25 14:10:07 +0900

    added css

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import React from "react";
 // import { selectClothes } from "../actions";
 import ClothesList from "../components/ClothesList";
-import ClothesDetail from "../components/ClothesDetail";
+import ClothesItems from "./ClothesItems";
+import "./App.css";
 // import AllClothesList from "../components/AllClothesList";
 
 const App = () => {
+  // const fetchImages = async ({searchTerm, per_page = 10}) => {
+  // try {
+  //   setError(false);
+  //     const { data } = await axios.get(
+  //       "https://api.unsplash.com/search/photos",
+  //       {
+  //         params: {
+  //           query: searchTerm,
+  //           per_page,
+  //         },
+  //         // params: { query: term},
+  //         headers: {
+  //           Authorization:
+  //             "Client-ID lk1sHfESga4bPGg0Xt0EbcMzx1IcuYw0uThfuucVPmg",
+  //         },
+  //       }
+  //     );
+
+  // const image=fetchImages({searchTerm: 'dress', per_page:1})
+
   return (
-    <div>
+    <div className="component">
       <ClothesList />
-      <ClothesDetail />
+      <ClothesItems />
     </div>
   );
 };
