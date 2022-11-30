f0e29e7c836606670b5303e9fd9673507f9a9b0f
marikomands <marikomands@gmail.com>
2022-11-30 14:46:55 +0900

    paassing item to ClothesItem component

--- src/components/ClothesItems.js
+++ src/components/ClothesItems.js
@@ -1,44 +1,25 @@
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import "./ClothesList.css";
-import { selectClothes } from "../actions";
 
-// import AllClothesList from "./AllClothesList";
+import ClothesItem from "./ClothesItem";
 
-const ClothesItems = ({ items, selectClothes, searchTerm, per_page }) => {
-  useEffect(() => {
-    const items = async () => {
-      const { data } = await axios.get(
-        "https://api.unsplash.com/search/photos",
-        {
-          params: { query: searchTerm, per_page: 2 },
-          headers: {
-            Authorization:
-              "Client-ID fmAnr5mkf9QbV8cgJu59tZSYYSH_wyyZwquCAu-7S2M",
-          },
-        }
-      );
-      selectClothes(data);
-    };
-  }, []);
+const ClothesItems = ({ items }) => {
+  console.log("🚀 ~ ClothesItems ~ items", items);
 
-  if (!items) {
-    return <div>Secelct an item</div>;
-  }
-
-  return items.map((item) => {
-    return (
-      <div className="itemList" key={item.product}>
-        <h2>Product: {item.product}</h2>
-        <h2>Price: {item.price}</h2>
-      </div>
-    );
-  });
+  return (
+    <div className="ClothesItems">
+      {items?.length ? (
+        items.map((item) => {
+          return <ClothesItem key={item.product} item={item} />;
+        })
+      ) : (
+        <p>No items! Select a category.</p>
+      )}
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-  // console.log("🚀 ~ mapStateToProps ~ state", state);
   return { items: state.selectedTypeByCategory };
 };
-export default connect(mapStateToProps, { selectClothes })(ClothesItems);
+export default connect(mapStateToProps)(ClothesItems);
