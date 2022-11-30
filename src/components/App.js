09578aa5b015be12f0878f227714f6c0dce1a0db
marikomands <marikomands@gmail.com>
2022-11-30 14:43:57 +0900

    changed the  name of component to ClothesCategories

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,12 @@
 import React from "react";
-// import { selectClothes } from "../actions";
-import ClothesList from "../components/ClothesList";
+import ClothesCategories from "../components/ClothesCategories";
 import ClothesItems from "./ClothesItems";
 import "./App.css";
-// import AllClothesList from "../components/AllClothesList";
 
 const App = () => {
-  // const fetchImages = async ({searchTerm, per_page = 10}) => {
-  // try {
-  //   setError(false);
-  //     const { data } = await axios.get(
-  //       "https://api.unsplash.com/search/photos",
-  //       {
-  //         params: {
-  //           query: searchTerm,
-  //           per_page,
-  //         },
-  //         // params: { query: term},
-  //         headers: {
-  //           Authorization:
-  //             "Client-ID lk1sHfESga4bPGg0Xt0EbcMzx1IcuYw0uThfuucVPmg",
-  //         },
-  //       }
-  //     );
-
-  // const image=fetchImages({searchTerm: 'dress', per_page:1})
-
   return (
-    <div className="component">
-      <ClothesList />
+    <div className="App">
+      <ClothesCategories />
       <ClothesItems />
     </div>
   );
